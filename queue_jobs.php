059c356c8033692dc0a849d3e79110dbfa7c440a
Danny Panzer <danny.panzer@microblink.com>
2021-09-12 00:12:44 -0400

    accept command line arg as sleep time

--- queue_jobs.php
+++ queue_jobs.php
@@ -6,8 +6,17 @@ Resque::setBackend(getenv("REDIS_HOST") . ":" . getenv("REDIS_PORT"));
 
 $numJobs = 1000;
 
+$sleepTime = $argc > 1 ? $argv[1] : 5;
+
 for ($i = 0; $i < $numJobs; $i++) {
-    Resque::enqueue("redis_test_jobs", "RedisTestJob", ['job_id' => $i]);
+    Resque::enqueue(
+        "redis_test_jobs",
+        "RedisTestJob",
+        [
+            'job_id' => $i,
+            'sleep_time' => $sleepTime
+        ]
+    );
 }
 
 echo "Finished queueing {$numJobs} jobs\n";
