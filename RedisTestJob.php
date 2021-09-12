1e1ae82c8e0e7d696e464456085a0fc2dba0fa16
Danny Panzer <danny.panzer@microblink.com>
2021-09-12 00:18:20 -0400

    fix typo

--- RedisTestJob.php
+++ RedisTestJob.php
@@ -3,7 +3,7 @@
 class RedisTestJob {
     public function perform() {
         $jobId = $this->args['job_id'];
-        $sleepTime = $this->args['sleep_time']
+        $sleepTime = $this->args['sleep_time'];
 
         echo "Job {$jobId} going to sleep for {$sleepTime}\n";
         sleep($sleepTime);
